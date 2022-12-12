5349fc48e81c4145bf81d9a4312f93cf7f9d63cc
DalidovichOlga <119608412+DalidovichOlga@users.noreply.github.com>
2022-12-12 09:17:32 +0300

    задание 3

---
+++ questionnaire3/src/main/java/ru/otus/spring/dao/QuestionDaoCsv.java
@@ -0,0 +1,52 @@
+package ru.otus.spring.dao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.otus.spring.domain.Question;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class QuestionDaoCsv implements IQuestionDao {
+    private List<Question> questionList = new ArrayList<>();
+    private boolean initiated = false;
+    private static String fileName; 
+
+    public QuestionDaoCsv(@Value("${csvfile.path}") String fileName )
+    {
+        this.fileName = fileName;
+    }
+
+    void readFileScv() {
+        if (initiated) return;
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream stream = classLoader.getResourceAsStream(fileName);
+        Stream<String> streamString = new BufferedReader(new InputStreamReader(stream))
+                .lines();
+        questionList = streamString.map(str -> {
+            String[] stringQuestion = str.split(",");
+            return new Question(stringQuestion[0], stringQuestion[1], stringQuestion[2]);
+        }).collect(Collectors.toList());
+        initiated = true;
+
+    }
+
+    @Override
+    public Question getQuestion(int index) {
+        readFileScv();
+        if (index < getCount())
+            return questionList.get(index);
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        readFileScv();
+        return questionList.size();
+    }
+}
